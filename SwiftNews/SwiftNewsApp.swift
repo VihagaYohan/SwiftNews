0604b3fa62ac3d810aa4de41083f447887d7b055
Vihaga Yohan <35211294+VihagaYohan@users.noreply.github.com>
2024-10-15 20:52:25 +0530

    Initial Commit

---
+++ SwiftNews/SwiftNewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftNewsApp.swift
+//  SwiftNews
+//
+//  Created by Yohan on 2024-10-15.
+//
+
+import SwiftUI
+
+@main
+struct SwiftNewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
